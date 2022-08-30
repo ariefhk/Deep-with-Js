c6e205b36c023781bce830447f505fbd27a19d3c
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-31 01:24:25 +0700

    add loop.js

---
+++ Learn/2_Looping/loop.js
@@ -0,0 +1,12 @@
+// while-loop
+let number = 10;
+while (number >= 1) {
+  console.log(`Angka ke ${number}`);
+  number--;
+}
+
+console.log("\n");
+// for-loop
+for (let x = 1; x <= 10; x++) {
+  console.log(`Angka ke ${x}`);
+}
